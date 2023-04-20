43c6e234376c8f578c0d98e8d0682aabf4a2d042
Mdmosley12 <mdmosley12@gmail.com>
2023-04-20 15:10:07 +0100

    Began styling header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,23 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header>
+      <div id="logoBox">
+        <h3>Logo</h3>
+      </div>
       <nav>
-        <img />
-        <Link href="/">Home</Link>
-        <Link href="/events">Events</Link>
-        <Link href="/about-us">About Us</Link>
+        <Link class="nav-button" href="/">
+          Home
+        </Link>
+        <Link class="nav-button" href="/events">
+          Events
+        </Link>
+        <Link class="nav-button" href="/about-us">
+          About Us
+        </Link>
       </nav>
+      <div id="signUpAndSearchBox">
+        <h3>Login/SignUp & Search Buttons</h3>
+      </div>
     </header>
   );
 };
